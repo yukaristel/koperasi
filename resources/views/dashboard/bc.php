d9ebbc090e6f139029dc2f354e478cca80950318
yukaristel <rokumitsu012@gmail.com>
2025-07-22 10:11:20 +0700

    dashboard sementara

--- resources/views/dashboard/bc.php
+++ resources/views/dashboard/bc.php
@@ -80,7 +80,7 @@
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
         <div class="container-fluid">
             <div class="row my-4 d-flex align-items-stretch">
-                <div class="col-lg-4 col-md-6 mb-md-0 mb-4">
+                <div class="col-lg-3 col-md-6 mb-md-0 mb-4">
                     <div class="card shadow-xs border h-100">
                         <div class="card-header pb-0">
                             <h6 class="font-weight-semibold text-lg mb-0">Simpanan Anggota</h6>
@@ -94,60 +94,7 @@
                         </div>
                     </div>
                 </div> <!--kiri-->
-                <div class="col-lg-4 col-md-6 mb-md-0 mb-4">
-                    <div class="card shadow-xs border h-100">
-                        <div class="card-header pb-0">
-                            <h6 class="font-weight-semibold text-lg mb-0">Pinjaman Anggota</h6>
-                            <p class="text-sm">Perkembangan pinjaman anggota koperasi.</p>
-                        </div>
-                        <div class="card-body py-3">
-                            <div class="chart mb-2">
-                                <canvas id="chart-pinjaman" class="chart-canvas" height="240"></canvas>
-                            </div>
-                            <button class="btn btn-white mb-0 ms-auto">Lihat Laporan</button>
-                        </div>
-                    </div>
-                </div> 
-                <div class="col-xl-4 col-sm-6 mb-md-0 mb-4">
-                    <div class="card border shadow-xs mb-4">
-                        <div class="card-body text-start p-3 w-100 d-flex justify-content-between align-items-center">
-                            <div>
-                                <div
-                                    class="icon icon-shape icon-sm bg-primary text-white text-center border-radius-sm d-flex align-items-center justify-content-center mb-3">
-                                    <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 24 24" fill="currentColor">
-                                        <path fill-rule="evenodd"
-                                            d="M5.25 2.25a3 3 0 00-3 3v4.318a3 3 0 00.879 2.121l9.58 9.581c.92.92 2.39 1.186 3.548.428a18.849 18.849 0 005.441-5.44c.758-1.16.492-2.629-.428-3.548l-9.58-9.581a3 3 0 00-2.122-.879H5.25zM6.375 7.5a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                                <p class="text-sm text-secondary mb-1">Jumlah Anggota</p>
-                                <h4 class="mb-2 font-weight-bold">1.235</h4>
-                                <div class="d-flex align-items-center">
-                                    <span class="text-sm text-success font-weight-bolder">
-                                        <i class="fa fa-chevron-up text-xs me-1"></i>18%
-                                    </span>
-                                    <span class="text-sm ms-1">dari 1.047</span>
-                                </div>
-                            </div>
-                            <div class="d-flex align-items-center justify-content-center"
-                                style="flex: 0 0 90px; height: 100%;">
-                                <a href="/data/anggota" class="btn-pulse-outer">
-                                    <span class="pulse-ring"></span>
-                                    <span class="pulse-ring delay"></span>
-                                    <span class="pulse-ring delay2"></span>
-                                    <i class="fas fa-play"></i>
-                                </a>
-
-
-                            </div>
-
-                        </div>
-                    </div>
-                </div> <!--kanan-->
-            </div>
-            <div class="row">
-                <div class="col-lg-12 col-md-6 mb-md-0 mb-4">
+                <div class="col-lg-6">
                     <div class="card shadow-xs border h-100">
                         <div class="card-header pb-0">
                             <div class="d-sm-flex align-items-center mb-3">
@@ -190,6 +137,151 @@
                         </div>
                     </div>
                 </div><!--tengah-->
+                <div class="col-lg-3 col-md-6 mb-md-0 mb-4">
+                    <div class="card shadow-xs border h-100">
+                        <div class="card-header pb-0">
+                            <h6 class="font-weight-semibold text-lg mb-0">Pinjaman Anggota</h6>
+                            <p class="text-sm">Perkembangan pinjaman anggota koperasi.</p>
+                        </div>
+                        <div class="card-body py-3">
+                            <div class="chart mb-2">
+                                <canvas id="chart-pinjaman" class="chart-canvas" height="240"></canvas>
+                            </div>
+                            <button class="btn btn-white mb-0 ms-auto">Lihat Laporan</button>
+                        </div>
+                    </div>
+                </div> <!--kanan-->
+            </div>
+            <div class="row">
+                <div class="col-xl-3 col-sm-6 mb-xl-0">
+                    <div class="card border shadow-xs mb-4">
+                        <div class="card-body text-start p-3 w-100">
+                            <div
+                                class="icon icon-shape icon-sm bg-primary text-white text-center border-radius-sm d-flex align-items-center justify-content-center mb-3">
+                                <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                    fill="currentColor">
+                                    <path d="M4.5 3.75a3 3 0 00-3 3v.75h21v-.75a3 3 0 00-3-3h-15z" />
+                                    <path fill-rule="evenodd"
+                                        d="M22.5 9.75h-21v7.5a3 3 0 003 3h15a3 3 0 003-3v-7.5zm-18 3.75a.75.75 0 01.75-.75h6a.75.75 0 010 1.5h-6a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5h3a.75.75 0 000-1.5h-3z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="w-100">
+                                        <p class="text-sm text-secondary mb-1">Total Simpanan</p>
+                                        <h4 class="mb-2 font-weight-bold">Rp 991.185.000</h4>
+                                        <div class="d-flex align-items-center">
+                                            <span class="text-sm text-success font-weight-bolder">
+                                                <i class="fa fa-chevron-up text-xs me-1"></i>10.5%
+                                            </span>
+                                            <span class="text-sm ms-1">dari Rp 897.400.000</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6 mb-xl-0">
+                    <div class="card border shadow-xs mb-4">
+                        <div class="card-body text-start p-3 w-100">
+                            <div
+                                class="icon icon-shape icon-sm bg-primary text-white text-center border-radius-sm d-flex align-items-center justify-content-center mb-3">
+                                <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                    fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                        d="M7.5 5.25a3 3 0 013-3h3a3 3 0 013 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0112 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 017.5 5.455V5.25zm7.5 0v.09a49.488 49.488 0 00-6 0v-.09a1.5 1.5 0 011.5-1.5h3a1.5 1.5 0 011.5 1.5zm-3 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                                        clip-rule="evenodd" />
+                                    <path
+                                        d="M3 18.4v-2.796a4.3 4.3 0 00.713.31A26.226 26.226 0 0012 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 01-6.477-.427C4.047 21.128 3 19.852 3 18.4z" />
+                                </svg>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="w-100">
+                                        <p class="text-sm text-secondary mb-1">Total Pinjaman</p>
+                                        <h4 class="mb-2 font-weight-bold">Rp 376.000.000</h4>
+                                        <div class="d-flex align-items-center">
+                                            <span class="text-sm text-success font-weight-bolder">
+                                                <i class="fa fa-chevron-up text-xs me-1"></i>55%
+                                            </span>
+                                            <span class="text-sm ms-1">dari Rp 243.000.000</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6 mb-xl-0">
+                    <div class="card border shadow-xs mb-4">
+                        <div class="card-body text-start p-3 w-100">
+                            <div
+                                class="icon icon-shape icon-sm bg-primary text-white text-center border-radius-sm d-flex align-items-center justify-content-center mb-3">
+                                <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                    fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                        d="M3 6a3 3 0 013-3h12a3 3 0 013 3v12a3 3 0 01-3 3H6a3 3 0 01-3-3V6zm4.5 7.5a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0v-2.25a.75.75 0 01.75-.75zm3.75-1.5a.75.75 0 00-1.5 0v4.5a.75.75 0 001.5 0V12zm2.25-3a.75.75 0 01.75.75v6.75a.75.75 0 01-1.5 0V9.75A.75.75 0 0113.5 9zm3.75-1.5a.75.75 0 00-1.5 0v9a.75.75 0 001.5 0v-9z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="w-100">
+                                        <p class="text-sm text-secondary mb-1">SHU Tahun Ini</p>
+                                        <h4 class="mb-2 font-weight-bold">Rp 450.530.000</h4>
+                                        <div class="d-flex align-items-center">
+                                            <span class="text-sm text-success font-weight-bolder">
+                                                <i class="fa fa-chevron-up text-xs me-1"></i>22%
+                                            </span>
+                                            <span class="text-sm ms-1">dari Rp 369.300.000</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-xl-3 col-sm-6">
+                    <div class="card border shadow-xs mb-4">
+                        <div class="card-body text-start p-3 w-100 d-flex justify-content-between align-items-center">
+                            <div>
+                                <div
+                                    class="icon icon-shape icon-sm bg-primary text-white text-center border-radius-sm d-flex align-items-center justify-content-center mb-3">
+                                    <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24" fill="currentColor">
+                                        <path fill-rule="evenodd"
+                                            d="M5.25 2.25a3 3 0 00-3 3v4.318a3 3 0 00.879 2.121l9.58 9.581c.92.92 2.39 1.186 3.548.428a18.849 18.849 0 005.441-5.44c.758-1.16.492-2.629-.428-3.548l-9.58-9.581a3 3 0 00-2.122-.879H5.25zM6.375 7.5a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                                <p class="text-sm text-secondary mb-1">Jumlah Anggota</p>
+                                <h4 class="mb-2 font-weight-bold">1.235</h4>
+                                <div class="d-flex align-items-center">
+                                    <span class="text-sm text-success font-weight-bolder">
+                                        <i class="fa fa-chevron-up text-xs me-1"></i>18%
+                                    </span>
+                                    <span class="text-sm ms-1">dari 1.047</span>
+                                </div>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-center"
+                                style="flex: 0 0 90px; height: 100%;">
+                                <a href="/data/anggota" class="btn-pulse-outer">
+                                    <span class="pulse-ring"></span>
+                                    <span class="pulse-ring delay"></span>
+                                    <span class="pulse-ring delay2"></span>
+                                    <i class="fas fa-play"></i>
+                                </a>
+
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
@@ -380,7 +472,6 @@
                 datasets: [{
                         label: "Pendapatan",
                         type: "bar",
-                        order: 1,
                         tension: 0.4,
                         borderWidth: 0,
                         borderSkipped: false,
@@ -391,12 +482,11 @@
                     {
                         label: "Beban",
                         type: "bar",
-                        order: 1,
                         tension: 0.4,
                         borderWidth: 0,
                         borderSkipped: false,
                         backgroundColor: "#ed3a7c",
-                        data: @json(array_map(fn($v) =>  $v, array_values($saldo[5]))),
+                        data: @json(array_map(fn($v) => -1 * $v, array_values($saldo[5]))),
                         maxBarThickness: 6
                     },
                     {
@@ -452,7 +542,7 @@
                 },
                 scales: {
                     y: {
-                        stacked: false,
+                        stacked: true,
                         grid: {
                             drawBorder: false,
                             display: true,
@@ -476,7 +566,7 @@
                         }
                     },
                     x: {
-                        stacked: false,
+                        stacked: true,
                         grid: {
                             drawBorder: false,
                             display: false,
